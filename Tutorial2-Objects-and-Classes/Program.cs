a18b16be2bb55047103b2c10f53ba12990079bab
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-24 09:04:24 -0500

    Add project files.

---
+++ Tutorial2-Objects-and-Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * OOP Tutorial 2
+ * A Review on Objects and Classes (Brings a C++ Program into C#)
+ * 
+ * @author  Nick Sturch-Flint
+ * @since   2021-01-24
+ * @version 1.0.0
+ * 
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial2_Objects_and_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
